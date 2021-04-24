642a3b8c02e2b7a9a1796209cdba2145a7df2345
hk7math <hk7math@gmail.com>
2021-04-24 16:27:51 +0800

    submit + postdata

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,15 @@ class EatZi extends StatefulWidget {
 }
 
 class _EatZiState extends State<EatZi> {
-  TextEditingController textController = TextEditingController();
+  final String baseUrl =
+      'https://script.google.com/macros/s/AKfycbzK9arWky9kB_d8bPsmRSVtbUaZJl5xrG7H4qpDW4VAoG9CVW3kAJPiL4HzP24xExaUIQ/exec';
+  final TextEditingController textController = TextEditingController();
+  final TextEditingController textController2 = TextEditingController();
+  final FocusNode textNode = FocusNode();
+  bool isOk = true;
+
   Future<List<Word>> getWord() async {
-    Response res = await get(Uri.parse(
-        'https://script.google.com/macros/s/AKfycbxtIPYeloaPye8qVIxYaTs4w1lixcDo_fsSmjx5Fa3zCtG7Q-yN1u_K3hG2XFUvQjidYA/exec'));
+    Response res = await get(Uri.parse(baseUrl));
     if (res.statusCode == 200) {
       List<dynamic> body = jsonDecode(res.body);
       List<Word> words = body.map((e) => Word.fromJson(e)).toList();
@@ -28,6 +33,28 @@ class _EatZiState extends State<EatZi> {
     } else {
       throw 'Sick jor';
     }
+  }
+
+  Future<List<Word>> postWord(String word, String eatzi) async {
+    Response res = await post(Uri.parse(baseUrl + '?word=$word&eatzi=$eatzi'));
+    if (res.statusCode == 200) {
+      List<dynamic> body = jsonDecode(res.body);
+      List<Word> words = body.map((e) => Word.fromJson(e)).toList();
+      return words;
+    } else {
+      throw 'Sick jor';
+    }
+  }
+
+  void onSubmit(EatZi widget) async {
+    unlockNotifier.value = false;
+    String word = textController.value.text;
+    String eatzi = textController2.value.text;
+    textController2.clear();
+    widget.words = await postWord(word, eatzi);
+    unlockNotifier.value = true;
+    inputNotifier.value = word;
+    inputNotifier.notifyListeners();
   }
 
   @override
@@ -52,91 +79,113 @@ class _EatZiState extends State<EatZi> {
                                       .contains(input.toString().toLowerCase()))
                                   .toList()
                                     ..shuffle())
-                              .take(10)
+                              .take(12)
                               .toList();
                           return Expanded(
-                            child: GridView.count(
-                              crossAxisCount: 3,
-                              children: words.map((word) {
-                                Color color = randomColor();
-                                return FlipCard(
-                                    onFlipDone: (done) {
-                                      if (!done) {
-                                        textController.text = word.word;
-                                      }
-                                    },
-                                    front: CardContainer(
-                                        color: color, texts: [word.word]),
-                                    back: CardContainer(
-                                        color: color.withOpacity(.5),
-                                        texts: word.eatzi
-                                            .map((e) =>
-                                                widget.words[int.parse(e)].word)
-                                            .toList()));
-                              }).toList(),
+                            child: SizedBox(
+                              width:
+                                  min(MediaQuery.of(context).size.width, 600),
+                              child: GridView.count(
+                                crossAxisCount: 3,
+                                children: words.map((word) {
+                                  Color color = randomColor();
+                                  return FlipCard(
+                                      onFlipDone: (done) {
+                                        if (!done) {
+                                          textController.text = word.word;
+                                          textNode.requestFocus();
+                                        }
+                                      },
+                                      front: CardContainer(
+                                          color: color, texts: [word.word]),
+                                      back: CardContainer(
+                                          color: color.withOpacity(.5),
+                                          texts: word.eatzi
+                                              .map((e) => widget
+                                                  .words[int.parse(e)].word)
+                                              .toList()));
+                                }).toList(),
+                              ),
                             ),
                           );
                         },
                       );
                     } else {
                       return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: CircularProgressIndicator(),
+                        padding: const EdgeInsets.all(100.0),
+                        child: SizedBox(
+                          width: 150,
+                          height: 150,
+                          child: CircularProgressIndicator(),
+                        ),
                       );
                     }
                   }),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                        flex: 5,
-                        child: TextField(
-                          controller: textController,
-                          onChanged: (input) {
-                            inputNotifier.value = input;
-                          },
-                          decoration: InputDecoration(
-                            contentPadding: const EdgeInsets.all(8.0),
-                            border: OutlineInputBorder(),
-                            labelText: '食字之橋',
-                            hintText: '宣任',
+              ValueListenableBuilder(
+                valueListenable: unlockNotifier,
+                builder: (context, unlock, child) => Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Flexible(
+                          flex: 5,
+                          child: TextField(
+                            enabled: unlock,
+                            controller: textController,
+                            onChanged: (input) {
+                              inputNotifier.value = input;
+                            },
+                            decoration: InputDecoration(
+                              contentPadding: const EdgeInsets.all(8.0),
+                              border: OutlineInputBorder(),
+                              labelText: '食字之橋',
+                            ),
                           ),
                         ),
-                      ),
-                      Spacer(
-                        flex: 1,
-                      ),
-                      Flexible(
-                        flex: 5,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            contentPadding: const EdgeInsets.all(8.0),
-                            border: OutlineInputBorder(),
-                            labelText: '伸出對手',
-                            hintText: '專一',
+                        Spacer(
+                          flex: 1,
+                        ),
+                        Flexible(
+                          flex: 5,
+                          child: TextField(
+                            enabled: unlock,
+                            controller: textController2,
+                            focusNode: textNode,
+                            onSubmitted: (text) {
+                              onSubmit(widget);
+                            },
+                            decoration: InputDecoration(
+                              contentPadding: const EdgeInsets.all(8.0),
+                              border: OutlineInputBorder(),
+                              labelText: '執字之手',
+                            ),
                           ),
                         ),
-                      ),
-                      Spacer(flex: 1),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            minimumSize: Size(50, 50), primary: randomColor()),
-                        child: Icon(
-                          Icons.send,
-                          color: Colors.white,
+                        Spacer(flex: 1),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              minimumSize: Size(50, 50),
+                              primary: randomColor()),
+                          child: Icon(
+                            Icons.send,
+                            color: Colors.white,
+                          ),
+                          onPressed: unlock
+                              ? () {
+                                  onSubmit(widget);
+                                }
+                              : () {},
                         ),
-                        onPressed: () {},
-                      ),
-                    ]),
+                      ]),
+                ),
               ),
             ])));
   }
 }
 
 Color randomColor() =>
-    Color(Random().nextInt(0xFFFFFF)).withOpacity(.2);
+    Color(Random().nextInt(0xFFFFFFFF)).withAlpha(0xFF).withOpacity(.2);
 
 class CardContainer extends StatelessWidget {
   const CardContainer({
@@ -151,7 +200,7 @@ class CardContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(20.0),
+      padding: EdgeInsets.all(8.0),
       child: Container(
           decoration: BoxDecoration(
             color: color,
@@ -173,3 +222,4 @@ class CardContainer extends StatelessWidget {
 }
 
 ValueNotifier inputNotifier = ValueNotifier('');
+ValueNotifier unlockNotifier = ValueNotifier(true);
